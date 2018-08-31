9f0c23c9ed33a857f629b6704e67fd086ab65437
Matias Seijas <matiseijas@gmail.com>
2018-08-31 10:49:42 -0400

    Init project

---
+++ CryptoWorkshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoWorkshop
+//
+//  Created by Matias Seijas on 8/31/18.
+//  Copyright © 2018 TanookiLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
