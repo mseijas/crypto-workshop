7741bc03cf81daf1ce8e860a8dcff98803866eff
Matias Seijas <matiseijas@gmail.com>
2018-08-31 11:33:29 -0400

    Implement UI

--- CryptoWorkshop/ViewController.swift
+++ CryptoWorkshop/ViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var assetCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
