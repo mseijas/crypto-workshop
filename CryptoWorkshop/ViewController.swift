929b1d55758b90f6ba055d99f76b202662bdc52c
Matias Seijas <matiseijas@gmail.com>
2018-08-31 17:32:26 -0400

    Add method stubs

--- CryptoWorkshop/ViewController.swift
+++ CryptoWorkshop/ViewController.swift
@@ -12,14 +12,52 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var assetCountLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let tokenMetadataProvider = TokenMetadaProvider()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        let web3 = setupWeb3()
         
-        for _ in 0...5 {
-            let imageView = ImageURLView(imageUrl: URL(string: "https://storage.googleapis.com/opensea-prod.appspot.com/0x06012c8cf97bead5deae237070f9587f8e7a266d/899656.svg")!)
-            stackView.addArrangedSubview(imageView)
-        }
+        // CryptoStrikers contract
+        
+        // Ethreum user account
+        
+        // Populate count label
+        
+        // Get Token metadata for each token
+        
+        // Render UI
+
+        activityIndicator.stopAnimating()
+    }
+    
+    // 1. Setup Ethereum node + web3 object
+    func setupWeb3() -> web3 {
+        
     }
 
+    // 2. Setup contract with ABI
+    func setupContract(with web3: web3, address: EthereumAddress) -> web3.web3contract {
+        
+    }
+    
+    // 3. Interact with the contract
+    // 3a. Get token balance for an account
+    func tokenBalance(for account: EthereumAddress, on contract: web3.web3contract) -> Int {
+        
+    }
+    
+    // 3b. Get token id by index
+    func tokenId(for account: EthereumAddress, index: Int, on contract: web3.web3contract) -> Int {
+        
+    }
+    
+    // 3c. Get token metadata url by id
+    func tokenMetadataURL(id: Int, on contract: web3.web3contract) -> URL {
+        
+    }
+    
 }
